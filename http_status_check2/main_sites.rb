6169456a42bf1cdd818c4c152a8778a6d522b876
hikao <fukuzawh0322@gmail.com>
2015-10-08 23:25:49 +0900

    Add statu checker

--- http_status_check2/main_sites.rb
+++ http_status_check2/main_sites.rb
@@ -4,44 +4,29 @@ require './status_checker'
 
 
 # 障害時に本番環境の主要サイトのステータスを確認するテスト
-class TestMainsiteStatusCheck < StatusChecker
+class TestMainsiteStatusCheck < Test::Unit::TestCase
+  include StatusChecker
+  attr_accessor :fails
+  def setup
+    p 'setup'
+    @mutex = Mutex.new
+    @fails = ["a"]
+    p @fails.object_id
+  end
+
+  def teardown
+    p 'teardown'
+    p @fails
+  end
+
   URL_LIST = YAML.load_file('main_sites_urls.yml')
 
   def test_pc_urls
     p "test_pc_urls"
     urls = URL_LIST['pc']
-    http_status_check(urls[0])
-
-    Parallel.each(urls, in_thread:0) do |url|
-      http_status_check(url)
+    Parallel.each(urls, in_thread:4) do |url|
+      http_status_check(url, @fails)
     end
   end
-
-  # def test_sp_urls
-  #   urls = URL_LIST['sp']
-  #   
-  #   Parallel.each(urls, in_processes:4) do |url|
-  #     p url
-  #     response = Net::HTTP.get_response(URI.parse(url))
-  #     if response.code.to_i != 200
-  #       p response.code
-  #     end
-  #     # assert_equal(200, response.code.to_i, "\n[Status Error]\ncheck this url => #{url}")
-  #   end
-  # end
-
-  # def test_mb_urls
-  #   p "test_mb_urls"
-  #   urls = URL_LIST['mb']
-  #   
-  #   Parallel.each(urls, in_processes:4) do |url|
-  #     response = Net::HTTP.get_response(URI.parse(url))
-  #     if response.code.to_i != 200
-  #       p url
-  #       p response.code
-  #     end
-  #     # assert_equal(200, response.code.to_i)
-  #   end
-  # end
 end
 
--- http_status_check2/status_checker.rb
+++ http_status_check2/status_checker.rb
@@ -9,26 +9,21 @@ Bundler.require
 # 2xx系ならok
 # 3xx系なら再度リクエストを投げる
 # 4xx~5xxはassertion
-class StatusChecker < Test::Unit::TestCase
-  include Test::Unit::Assertions
+module StatusChecker
   include Requests
+  #include Test::Unit::Assertions
   # HTTP ステータスコードをチェックする
   # @param [String] url チェック対象のurl
-  def http_status_check(url)
-    response = get_response(url)
-    if response.code =~ /2[0-9]{2}/
-      p url, response.code
-    elsif response.code =~ /3[0-9]{2}/
-      p url, response.code
-      puts "redirect to #{response['location']}"
-      check_redirect_url(response['location'])
-    elsif response.code =~ /(4|5)[0-9]{2}/
-      puts "\n[Status Error]\ncheck this url => #{url}")
-    else
-      raise "Status Code is Invalid. \n Status:#{response.code}"
+  def http_status_check(url, fails)
+    p fails.object_id
+    p url
+    if url == "ng"
+      #@mutex.synchronize do
+        p "----error!!!-----"
+        fails << "error #{url}"
+        p fails
+      #end
     end
-
-    # assert_equal(200, response.code.to_i, "\n[Status Error]\ncheck this url => #{url}")
   end
 
   #private
