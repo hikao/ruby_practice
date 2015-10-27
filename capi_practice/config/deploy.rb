e61740c9fe5fd7037899a858e988b40f210b8b41
hikao <fukuzawh0322@gmail.com>
2015-10-27 23:41:05 +0900

    minor change of deploy.rb

--- capi_practice/config/deploy.rb
+++ capi_practice/config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'https://github.com/hikao/ruby_practice.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
