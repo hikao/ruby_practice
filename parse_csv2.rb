0447a2a901be42e4a386e37bdd658721fd978df6
hikao <fukuzawh0322@gmail.com>
2015-11-14 00:15:39 +0900

    csv part3

--- parse_csv2.rb
+++ parse_csv2.rb
@@ -3,5 +3,18 @@
 require 'csv'
 
 p CSV.parse(%(1,2.5,2015-11-13), converters: :date)
+p CSV.parse(%(1,2.5,2015-11-13), converters: :float)
+p CSV.parse(%(1,2.5,2015-11-13), converters: :all)
 
+converter = ->(field, field_info) {
+  if field_info.index == 1
+    field.upcase rescue field
+  else
+    field
+  end
+}
+
+# 2つめが大文字になる
+p converter
+p CSV.parse(%(foo,bar,baz), converters: converter)
 
