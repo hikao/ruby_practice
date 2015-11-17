2ac482a2119b84e4907ff2bf944d1dd22fb5332f
hikao <fukuzawh0322@gmail.com>
2015-11-17 23:52:53 +0900

    pattern match part2

--- pattern_match.rb
+++ pattern_match.rb
@@ -6,3 +6,15 @@ p $&
 
 puts "Hello, This is a practice of regexp in ruby.".match(/ruby/)
 p $&
+
+puts '======='
+
+str = "Hi, Ruby! This is funtastic language! isn't it?"
+str.match(/language/)
+
+# match前
+puts $`
+# match後のテキスト
+puts $'
+# matchしたテキスト自身
+puts $&
