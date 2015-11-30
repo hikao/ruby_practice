514070b706df20c06f6618157064e6fc68e20c62
hikao <fukuzawh0322@gmail.com>
2015-11-30 23:53:56 +0900

    minor change of prime number

--- sosuu.rb
+++ sosuu.rb
@@ -2,9 +2,9 @@
 
 (2..100).each do |hoge|
   sqrt = Math.sqrt(hoge)
-  p sqrt
-  factor_found = (2..sqrt).any? {|i| hoge % i == 0}
+  factor_found = (2..sqrt).any? { |i| hoge % i == 0 }
+  p factor_found
   if factor_found then 
-	p "#{factor_found} is sosuu."
+	  p "#{factor_found} is sosuu."
   end
 end
