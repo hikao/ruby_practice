3abb17d5b10f7d295758cab06ff85b24b2bf71ad
hikao <fukuzawh0322@gmail.com>
2015-12-01 00:25:53 +0900

    add sample of prime num

---
+++ prime_num.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+def prime?(num)
+  # num > 0 をチェックすべき...
+  return false if num == 1
+  (2..Math.sqrt(num)).each do |x|
+    if num % x == 0
+      return false
+    end
+  end
+  true
+end
+
+(1..100).each do |n|
+  puts n if prime?(n)
+end
