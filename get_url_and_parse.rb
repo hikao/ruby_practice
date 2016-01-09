902429e9f85bfe75ac61fdd7c99280ccd117ceee
hikao <fukuzawh0322@gmail.com>
2016-01-10 00:21:51 +0900

    modify scrayping script.

--- get_url_and_parse.rb
+++ get_url_and_parse.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/ruby
 
+# githubリポジトリからRuby trendを取得
 require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open('https://github.com/trending?l=ruby'))
-doc.xpath('//li[@class="repo-list-item"]//h3[@class="repo-list-name"]//span[@class="prefix"]').each do |ele|
-  p ele.class
+doc.xpath('//li[@class="repo-list-item"]//h3[@class="repo-list-name"]//a').each do |ele|
+  ele.text.match(' ([\-0-9a-zA-Z]*?)\\n')
+  p $1
 end
