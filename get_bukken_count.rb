fa386b89130723f090e2f9c4a76c8e3703c6e4b9
hikao <fukuzawh0322@gmail.com>
2016-02-24 23:59:05 +0900

    modified

--- get_bukken_count.rb
+++ get_bukken_count.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 require 'uri'
 require 'cgi'
 require 'date'
+require 'json'
 
 target_url = URI.escape("http://www.homes.co.jp/")
 doc = Nokogiri::HTML(open(target_url))
@@ -13,4 +14,7 @@ bukken_count = doc.xpath("//p[@class='raCount']/span[@class='count']/text()")
 today = Date.today.strftime("%Y-%m-%d")
 puts "#{today} is #{bukken_count} bukken."
 
+# JSONを作成
+puts JSON.generate({today => bukken_count})
+ 
 # DBへ件数を登録する
